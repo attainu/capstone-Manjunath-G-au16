c5f6c19d5989405deabbf087f21afb947822b226
modimanju <modimanju2019@gmail.com>
2021-08-05 11:27:17 +0530

    userSchema

---
+++ Server/src/models/userSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+});
+
+
+const User = mongoose.model("USER", userSchema);
+
+module.exports = User;
