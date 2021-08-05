a339c4fd1adf69444193334d7f5a4955030477f8
modimanju <modimanju2019@gmail.com>
2021-08-05 11:36:18 +0530

    Bcrypt password

--- Server/src/models/userSchema.js
+++ Server/src/models/userSchema.js
@@ -25,6 +25,14 @@ const userSchema = new mongoose.Schema({
     ],
 });
 
+//Bcrypt(hashing) password
+//------------------------
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
 
 const User = mongoose.model("USER", userSchema);
 
