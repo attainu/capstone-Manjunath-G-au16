2e77a3a1a371f346e7b30ae7307e9ad1f83310b1
modimanju <modimanju2019@gmail.com>
2021-08-05 11:28:21 +0530

    Registration functionality created

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,11 +1,49 @@
-const express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) =>{
-    res.send("Hello from the Server");
-})
+const User = require("./models/userSchema");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
-app.listen(port,()=>{
-    console.log(`connection is live at port no. ${port}`);
-})
+dotenv.config({ path: "./config.env" });
+
+require("./db/conn");
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello from Server");
+});
+
+//User Registration
+//-----------------------------------------
+app.post("/register", async (req, res) => {
+    const { name, email, password, cpassword } = req.body;
+    if (!name || !email || !password || !cpassword) {
+        return res.status(422).json({ Error: "plz fill the field properly" });
+    }
+    try {
+        const userExist = await User.findOne({
+            email: email
+        });
+        if (userExist) {
+            return res.status(422).json({ Error: "User already Exists" });
+        } else if (password != cpassword) {
+            return res.status(422).json({ Error: "Passwords are not matching " });
+        } else {
+            const user = new User({ name, email, password });
+            await user.save();
+            res.status(201).json({ message: "User registered successfully" });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running at port no ${PORT}`);
+});
